b90247b5b62bf67d8485c4d78b4e9ac59880f0a0
Monika <lumimonika1@gmail.com>
2024-03-21 11:39:54 -1200

    Send and email an email with title and descripton

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import requests
+from send_email import send_email
 
 api_key = "a8715c41714640fda4107c82e621bba4"
 url = "https://newsapi.org/v2/everything?q=tesla&from=2024-02-21&sortBy=publishedAt&apiKey=a8715c41714640fda4107c82e621bba4"
@@ -10,6 +11,10 @@ request = requests.get(url)
 content = request.json()
 
 #Acces the article titles and decription
+body = " "
 for article in content["articles"]:
-    print(article["title"])
-    print(article["description"])
+    if article["title"] is not None:
+        body = body + article["title"] + "\n" + article["description"] + 2*"\n"
+
+body = body.encode("utf-8")
+send_email(message=body)
